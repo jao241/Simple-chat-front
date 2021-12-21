4150a1afcbfd5a2ae3d4d1e37e19e036dd660150
joao vitor <vorque45@gmail.com>
2021-12-21 17:45:03 -0300

    adicionei um input no componente

--- src/components/InputMessageField.vue
+++ src/components/InputMessageField.vue
@@ -1,5 +1,8 @@
 <template>
-    <section id="inputMessageField">Campo de input</section>
+    <section id="inputMessageField">
+      <input type="text" name="input" id="input" placeholder="Digite algo!">
+      <p>botao</p>
+    </section>
 </template>
 
 <script>
@@ -10,8 +13,20 @@ export default {
 
 <style scoped>
 
+section{
+  display: flex;
+}
+
 #inputMessageField{
   background-color: lightgray;
+  padding: 10px;
+}
+
+section input{
+  width: 90%;
+  padding: 2px;
+  outline: none;
+  cursor: pointer;
 }
 
 </style>
